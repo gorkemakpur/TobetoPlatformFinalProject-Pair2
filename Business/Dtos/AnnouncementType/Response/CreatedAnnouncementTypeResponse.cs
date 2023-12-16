dc805b3f48e823f523d03a9b211a76673539e302
gorkemakpur <gorkemakpur@gmail.com>
2023-12-16 17:22:08 +0300

    AnnouncementType dto

---
+++ Business/Dtos/AnnouncementType/Response/CreatedAnnouncementTypeResponse.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Dtos.AnnouncementType.Response
+{
+    public class CreatedAnnouncementTypeResponse
+    {
+        public string Type { get; set; }
+    }
+}
---
+++ Business/Dtos/AnnouncementType/Response/GetListAnnouncementTypeResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Dtos.AnnouncementType.Response
+{
+    public class GetListAnnouncementTypeResponse
+    {
+        public string Type { get; set; }
+    }
+}
