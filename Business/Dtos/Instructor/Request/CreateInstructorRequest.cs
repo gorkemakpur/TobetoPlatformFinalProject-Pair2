9a130f89173f572abd43d05432d198fce6a61b25
gorkemakpur <gorkemakpur@gmail.com>
2023-12-16 16:27:39 +0300

    PhoneNumber prop changed

--- Business/Dtos/Instructor/Request/CreateInstructorRequest.cs
+++ Business/Dtos/Instructor/Request/CreateInstructorRequest.cs
@@ -13,7 +13,7 @@ namespace Business.Dtos.Instructor.Request
         public string Email { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
-        public int PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
         public string About { get; set; }
     }
 }
