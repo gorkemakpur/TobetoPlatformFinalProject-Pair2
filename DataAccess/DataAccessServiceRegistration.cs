1400852793c780e0b4a42e47f7d9fa0d48fe9fe2
gorkemakpur <gorkemakpur@gmail.com>
2023-12-16 16:28:42 +0300

    added

--- DataAccess/DataAccessServiceRegistration.cs
+++ DataAccess/DataAccessServiceRegistration.cs
@@ -16,12 +16,35 @@ namespace DataAccess
     {
         public static IServiceCollection AddDataAccessServices(this IServiceCollection services, IConfiguration configuration)
         {
-            //services.AddDbContext<NorthwindContext>(options => options.UseInMemoryDatabase("nArchitecture"));
 
             services.AddDbContext<TobetoPlatformFinalProjectContext>(options => options.UseSqlServer(configuration.GetConnectionString("TobetoPlatformFinalProject")));
 
             services.AddScoped<IInstructorDal, EfInstructorDal>();
-            //services.AddScoped<ICategoryDal, EfCategoryDal>();
+            services.AddScoped<IAnnouncementDal, EfAnnouncementDal>();
+            services.AddScoped<IAnnouncementTypeDal, EfAnnouncementTypeDal>();
+            services.AddScoped<IApplicationDal, EfApplicationDal>();
+            services.AddScoped<IAsyncCourseDal, EfAsyncCourseDal>();
+            services.AddScoped<IAsyncCourseContentDal, EfAsyncCourseContentDal>();
+            services.AddScoped<IAsyncCourseDepartmentDal, EfAsyncCourseDepartmentDal>();
+            services.AddScoped<ICategoryDal, EfCategoryDal>();
+            services.AddScoped<ICertificateDal, EfCertificateDal>();
+            services.AddScoped<ICompetenceDal, EfCompetenceDal>();
+            services.AddScoped<IDepartmentDal, EfDepartmentDal>();
+            services.AddScoped<IEducationDal, EfEducationDal>();
+            services.AddScoped<IExperienceDal, EfExperienceDal>();
+            services.AddScoped<ILanguageDal, EfLanguageDal>();
+            services.AddScoped<ILanguageLevelDal, EfLanguageLevelDal>();
+            services.AddScoped<ILanguageTypeDal, EfLanguageTypeDal>();
+            services.AddScoped<ISituationDal, EfSituationDal>();
+            services.AddScoped<ISocialMediaDal, EfSocialMediaDal>();
+            services.AddScoped<ISocialMediaNameDal, EfSocialMediaNameDal>();
+            services.AddScoped<IStudentDal, EfStudentDal>();
+            services.AddScoped<ISurveyDal, EfSurveyDal>();
+            services.AddScoped<ISyncCourseDal, EfSyncCourseDal>();
+            services.AddScoped<ISyncCourseContentDal, EfSyncCourseContentDal>();
+            services.AddScoped<ISyncCourseDepartmentDal, EfSyncCourseDepartmentDal>();
+            services.AddScoped<ISyncCourseInstructorDal, EfSyncCourseInstructorDal>();
+
 
             return services;
         }
