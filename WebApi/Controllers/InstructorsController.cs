daa0fc9c3f617c622bd1c423ddb667363ed53efa
gorkemakpur <gorkemakpur@gmail.com>
2023-12-16 16:29:03 +0300

    WebApi Added

---
+++ WebApi/Controllers/InstructorsController.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstracts;
+using Business.Dtos.Instructor.Request;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InstructorsController : ControllerBase
+    {
+        IInstructorService _instructorService;
+
+        public InstructorsController(IInstructorService instructorService)
+        {
+            _instructorService = instructorService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateInstructorRequest createInstructorRequest)
+        {
+            var result = await _instructorService.Add(createInstructorRequest);
+            return Ok(result);
+        }
+    }
+}
