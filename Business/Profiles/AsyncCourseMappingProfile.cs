989c511ddb928a55dd13375146144303e5c4e535
gorkemakpur <gorkemakpur@gmail.com>
2023-12-24 01:46:17 +0300

    AsyncCourse Crud init

---
+++ Business/Profiles/AsyncCourseMappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Profiles
+{
+    public class AsyncCourseMappingProfile : Profile
+    {
+        
+        ////create
+        //CreateMap<Announcement, CreateAnnouncementRequest>().ReverseMap();
+        //CreateMap<Announcement, CreatedAnnouncementResponse>().ReverseMap();
+        //CreateMap<CreatedAnnouncementResponse, CreateAnnouncementRequest>().ReverseMap();
+        ////list
+        //CreateMap<Announcement, GetListAnnouncementResponse>().ReverseMap();
+        //CreateMap<Announcement, GetByIdAnnouncementResponse>().ReverseMap();
+        //CreateMap<IPaginate<Announcement>, Paginate<GetListAnnouncementResponse>>();
+        //    //delete
+        //    CreateMap<DeleteAnnouncementRequest, DeletedAnnouncementResponse>().ReverseMap();
+        ////update
+        //CreateMap<Announcement, UpdateAnnouncementRequest>().ReverseMap();
+        //CreateMap<UpdateAnnouncementRequest, UpdatedAnnouncementResponse>().ReverseMap();
+    }
+}
---
+++ DataAccess/EntityConfiguration/AsyncCourseContentConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.EntityConfiguration
+{
+    public class AsyncCourseContentConfiguration : IEntityTypeConfiguration<AsyncCourseContent>
+    {
+        public void Configure(EntityTypeBuilder<AsyncCourseContent> builder)
+        {
+            //Tablo
+            builder.ToTable("AsyncCourses").HasKey(i => i.Id);
+            //Sütunlar
+            builder.Property(i => i.Id).HasColumnName("Id").IsRequired();
+            builder.Property(i => i.AsyncCourseId).HasColumnName("AsyncCourseId").IsRequired();
+            builder.Property(i => i.Name).HasColumnName("Name").IsRequired();
+            builder.Property(i => i.Url).HasColumnName("Url").IsRequired();
+            builder.Property(i => i.Language).HasColumnName("Language").IsRequired();
+            builder.Property(i => i.Category).HasColumnName("Category").IsRequired();
+            builder.Property(i => i.Subtype).HasColumnName("Subtype").IsRequired();
+            builder.Property(i => i.Producer).HasColumnName("Producer").IsRequired();
+            builder.Property(i => i.Description).HasColumnName("Description").IsRequired();
+
+            //İlişki
+            builder.HasOne(i => i.AsyncCourse);
+
+            builder.HasQueryFilter(i => !i.DeletedDate.HasValue);
+        }
+    }
+}
