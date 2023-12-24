4cf1d40a9342f655428e76dd5d49a4c725ec75d0
gorkemakpur <gorkemakpur@gmail.com>
2023-12-24 03:00:22 +0300

    Application Crud Operations added

--- WebApi/Controllers/ApplicationsController.cs
+++ WebApi/Controllers/ApplicationsController.cs
@@ -1,6 +1,8 @@
 ﻿using Business.Abstracts;
 using Business.Dtos.Announcement.Request;
+using Business.Dtos.AnnouncementType.Request;
 using Business.Dtos.Application.Request;
+using Core.DataAccess.Paging;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
@@ -18,6 +20,37 @@ namespace WebApi.Controllers
         public async Task<IActionResult> Add([FromBody] CreateApplicationRequest createApplicationRequest)
         {
             var result = await _applicationService.AddAsync(createApplicationRequest);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update([FromBody] UpdateApplicationRequest updateApplicationRequest)
+        {
+            var result = await _applicationService.UpdateAsync(updateApplicationRequest);
+            return Ok(result);
+        }
+
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] DeleteApplicationRequest deleteApplicationRequest)
+        {
+            var result = await _applicationService.DeleteAsync(deleteApplicationRequest);
+            return Ok(result);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            var result = await _applicationService.GetListAsync(pageRequest);
+            return Ok(result);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetById([FromQuery] Guid id)
+        {
+            var result = await _applicationService.GetByIdAsync(id);
             return Ok(result);
         }
     }
