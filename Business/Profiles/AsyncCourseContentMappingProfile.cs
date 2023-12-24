46797b98f01e882dbc2168e813020460d6a37ffa
gorkemakpur <gorkemakpur@gmail.com>
2023-12-24 03:00:53 +0300

    AsyncCourseContentMappingProfile added

---
+++ Business/Profiles/AsyncCourseContentMappingProfile.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Business.Dtos.Announcement.Request;
+using Business.Dtos.Announcement.Response;
+using Business.Dtos.AsyncCourseContent.Request;
+using Business.Dtos.AsyncCourseContent.Response;
+using Core.DataAccess.Paging;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Profiles
+{
+    public class AsyncCourseContentMappingProfile: Profile
+    {
+        public AsyncCourseContentMappingProfile()
+        {
+            //create
+            CreateMap<AsyncCourseContentMappingProfile, CreateAsyncCourseContentRequest>().ReverseMap();
+            CreateMap<AsyncCourseContentMappingProfile, CreatedAsyncCourseContentResponse>().ReverseMap();
+            CreateMap<CreatedAsyncCourseContentResponse, CreateAsyncCourseContentRequest>().ReverseMap();
+            //list
+            CreateMap<AsyncCourseContentMappingProfile, GetListAsyncCourseContentResponse>().ReverseMap();
+            CreateMap<AsyncCourseContentMappingProfile, GetByIdAsyncCourseContentResponse>().ReverseMap();
+            CreateMap<IPaginate<AsyncCourseContentMappingProfile>, Paginate<GetListAsyncCourseContentResponse>>();
+            //delete
+            CreateMap<DeleteAsyncCourseContentRequest, DeletedAsyncCourseContentResponse>().ReverseMap();
+            //update
+            CreateMap<AsyncCourseContentMappingProfile, UpdateAsyncCourseContentRequest>().ReverseMap();
+            CreateMap<UpdateAsyncCourseContentRequest, UpdatedAsyncCourseContentResponse>().ReverseMap();
+        }
+    }
+}
