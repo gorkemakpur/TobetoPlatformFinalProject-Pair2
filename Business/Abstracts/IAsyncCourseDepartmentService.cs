77260104aff69cd8ef49a33b872238b63520a116
umudguny <ug152486@gmail.com>
2023-12-29 18:04:25 +0300

    AsyncCourseDepartment Crud

--- Business/Abstracts/IAsyncCourseDepartmentService.cs
+++ Business/Abstracts/IAsyncCourseDepartmentService.cs
@@ -1,8 +1,16 @@
-﻿namespace Business.Abstracts
+﻿using Business.Dtos.AsyncCourseDepartment.Request;
+using Business.Dtos.AsyncCourseDepartment.Response;
+using Core.DataAccess.Paging;
+
+namespace Business.Abstracts
 {
     public interface IAsyncCourseDepartmentService
     {
-        // İlgili metotlar
+        Task<CreatedAsyncCourseDepartmentResponse> AddAsync(CreateAsyncCourseDepartmentRequest createAsyncCourseDepartmentRequest);
+        Task<DeletedAsyncCourseDepartmentResponse> DeleteAsync(DeleteAsyncCourseDepartmentRequest deleteAsyncCourseDepartmentRequest);
+        Task<UpdatedAsyncCourseDepartmentResponse> UpdateAsync(UpdateAsyncCourseDepartmentRequest updateAsyncCourseDepartmentRequest);
+        Task<IPaginate<GetListAsyncCourseDepartmentResponse>> GetListAsync(PageRequest pageRequest);
+        Task<GetByIdAsyncCourseDepartmentResponse> GetByIdAsync(Guid id);
     }
 
 }
