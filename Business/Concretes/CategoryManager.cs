9d9425daa8c0f592555d25bdb468ec37a5738664
umudguny <ug152486@gmail.com>
2023-12-29 18:05:08 +0300

    Category Crud

--- Business/Concretes/CategoryManager.cs
+++ Business/Concretes/CategoryManager.cs
@@ -1,6 +1,15 @@
 ﻿using AutoMapper;
 using Business.Abstracts;
+using Business.Dtos.Category.Request;
+using Core.DataAccess.Paging;
+using Core.DataAccess.Repositories;
 using DataAccess.Abstracts;
+using Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using DataAccess.Concretes.EntityFramework;
+using Business.Dtos.Category.Response;
 
 namespace Business.Concretes
 {
@@ -15,7 +24,48 @@ namespace Business.Concretes
             _mapper = mapper;
         }
 
-        // İlgili metotlar
-    }
+        public async Task<CreatedCategoryResponse> AddAsync(CreateCategoryRequest createCategoryRequest)
+        {
+            Category addCategory = _mapper.Map<Category>(createCategoryRequest);
+            Category createdCategory = await _categoryDal.AddAsync(addCategory);
+            CreatedCategoryResponse createdCategoryResponse = _mapper.Map<CreatedCategoryResponse>(createdCategory);
+            return createdCategoryResponse;
+        }
 
+        public async Task<DeletedCategoryResponse> DeleteAsync(DeleteCategoryRequest deleteCategoryRequest)
+        {
+            Category removeCategory = await _categoryDal.GetAsync(predicate: c => c.Id == deleteCategoryRequest.Id);
+            await _categoryDal.DeleteAsync(removeCategory);
+            DeletedCategoryResponse deletedCategoryResponse = _mapper.Map<DeletedCategoryResponse>(deleteCategoryRequest);
+            return deletedCategoryResponse;
+        }
+
+        public async Task<GetByIdCategoryResponse> GetByIdAsync(Guid id)
+        {
+            Category result = await _categoryDal.GetAsync(predicate:c => c.Id == id);
+            GetByIdCategoryResponse data = _mapper.Map<GetByIdCategoryResponse>(result);
+            return data;
+        }
+
+        public async Task<IPaginate<GetListCategoryResponse>> GetListAsync(PageRequest pageRequest)
+        {
+            var data = await _categoryDal.GetListAsync(
+                index: 0, // pageRequest.PageIndex,
+                size: 10   // pageRequest.PageSize
+            );
+
+            var result = _mapper.Map<Paginate<GetListCategoryResponse>>(data);
+            return result;
+        }
+
+        public async Task<UpdatedCategoryResponse> UpdateAsync(UpdateCategoryRequest updateCategoryRequest)
+        {
+            Category updateCategory = await _categoryDal.GetAsync(predicate:c => c.Id == updateCategoryRequest.Id);
+            _mapper.Map(updateCategoryRequest, updateCategory);
+            Category updatedCategory = await _categoryDal.UpdateAsync(updateCategory);
+            UpdatedCategoryResponse updatedCategoryResponse = _mapper.Map<UpdatedCategoryResponse>(updatedCategory);
+            return updatedCategoryResponse;
+        }
+
+    }
 }
---
+++ DataAccess/EntityConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace DataAccess.EntityConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            // Tablomuz
+            builder.ToTable("Categories").HasKey(c => c.Id);
+
+            // Sütunlar
+            builder.Property(c => c.Id).HasColumnName("Id").IsRequired();
+            builder.Property(c => c.Name).HasColumnName("Name").IsRequired();
+
+            // İlişkiler
+            builder.HasMany(c => c.SyncCourses)
+                .WithOne(sc => sc.Category)
+                .HasForeignKey(sc => sc.CategoryId);
+
+
+
+            builder.HasMany(c => c.AsyncCourses)
+                .WithOne(ac => ac.Category)
+                .HasForeignKey(ac => ac.CategoryId);
+                
+
+            // Uniq Key
+            builder.HasIndex(c => c.Name).IsUnique();
+
+            builder.HasQueryFilter(c => !c.DeletedDate.HasValue);
+        }
+    }
+}
