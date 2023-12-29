e86321acfd5759488f9a38457993c76b60a5e393
umudguny <ug152486@gmail.com>
2023-12-29 18:07:26 +0300

    SyncCourseContent Crud

--- Business/Concretes/SyncCourseContentManager.cs
+++ Business/Concretes/SyncCourseContentManager.cs
@@ -1,21 +1,67 @@
 ﻿using AutoMapper;
 using Business.Abstracts;
+using Business.Dtos.SyncCourseContent.Request;
+using Business.Dtos.SyncCourseContent.Response;
+using Core.DataAccess.Paging;
 using DataAccess.Abstracts;
+using DataAccess.Concretes.EntityFramework;
+using Entities.Concretes;
 
-namespace Business.Concretes
+public class SyncCourseContentManager : ISyncCourseContentService
 {
-    public class SyncCourseContentManager : ISyncCourseContentService
+    private readonly ISyncCourseContentDal _syncCourseContentDal;
+    private readonly IMapper _mapper;
+
+    public SyncCourseContentManager(ISyncCourseContentDal syncCourseContentDal, IMapper mapper)
     {
-        private readonly ISyncCourseContentDal _syncCourseContentDal;
-        private readonly IMapper _mapper;
-
-        public SyncCourseContentManager(ISyncCourseContentDal syncCourseContentDal, IMapper mapper)
-        {
-            _syncCourseContentDal = syncCourseContentDal;
-            _mapper = mapper;
-        }
-
-        // İlgili metotlar
+        _syncCourseContentDal = syncCourseContentDal;
+        _mapper = mapper;
     }
 
+    public async Task<CreatedSyncCourseContentResponse> AddAsync(CreateSyncCourseContentRequest createSyncCourseContentRequest)
+    {
+        SyncCourseContent addSyncCourseContent = _mapper.Map<SyncCourseContent>(createSyncCourseContentRequest);
+        SyncCourseContent createdSyncCourseContent = await _syncCourseContentDal.AddAsync(addSyncCourseContent);
+        CreatedSyncCourseContentResponse createdSyncCourseContentResponse = _mapper.Map<CreatedSyncCourseContentResponse>(createdSyncCourseContent);
+        return createdSyncCourseContentResponse;
+    }
+
+    public async Task<DeletedSyncCourseContentResponse> DeleteAsync(DeleteSyncCourseContentRequest deleteSyncCourseContentRequest)
+    {
+        SyncCourseContent removeSyncCourseContent = await _syncCourseContentDal.GetAsync(predicate: c => c.Id == deleteSyncCourseContentRequest.Id);
+        await _syncCourseContentDal.DeleteAsync(removeSyncCourseContent);
+
+        DeletedSyncCourseContentResponse deletedSyncCourseContentResponse = _mapper.Map<DeletedSyncCourseContentResponse>(deleteSyncCourseContentRequest);
+        return deletedSyncCourseContentResponse;
+    }
+
+    public async Task<GetByIdSyncCourseContentResponse> GetByIdAsync(Guid id)
+    {
+
+        SyncCourseContent result = await _syncCourseContentDal.GetAsync(predicate:c => c.Id == id);
+        GetByIdSyncCourseContentResponse data = _mapper.Map<GetByIdSyncCourseContentResponse>(result);
+        return data;
+    }
+
+    public async Task<IPaginate<GetListSyncCourseContentResponse>> GetListAsync(PageRequest pageRequest)
+    {
+        var data = await _syncCourseContentDal.GetListAsync(
+            index: 0, // pageRequest.PageIndex,
+            size: 10   // pageRequest.PageSize
+        );
+
+        var result = _mapper.Map<Paginate<GetListSyncCourseContentResponse>>(data);
+        return result;
+    }
+
+    public async Task<UpdatedSyncCourseContentResponse> UpdateAsync(UpdateSyncCourseContentRequest updateSyncCourseContentRequest)
+    {
+        SyncCourseContent updateSyncCourseContent = await _syncCourseContentDal.GetAsync(predicate:c => c.Id == updateSyncCourseContentRequest.Id);
+        _mapper.Map(updateSyncCourseContentRequest, updateSyncCourseContent);
+
+        SyncCourseContent updatedSyncCourseContent = await _syncCourseContentDal.UpdateAsync(updateSyncCourseContent);
+        UpdatedSyncCourseContentResponse updatedSyncCourseContentResponse = _mapper.Map<UpdatedSyncCourseContentResponse>(updatedSyncCourseContent);
+
+        return updatedSyncCourseContentResponse;
+    }
 }
---
+++ Business/Dtos/SyncCourseContent/Response/GetByIdSyncCourseContentResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Dtos.SyncCourseContent.Response
+{
+    public class GetByIdSyncCourseContentResponse
+    {
+        public Guid Id { get; set; }
+        public Guid SyncCourseId { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public DateTime StartedDate { get; set; }
+        public DateTime EndedDate { get; set; }
+    }
+}
