af57a44ab91b25ffe611248694bd6f7d10fbe93b
umudguny <ug152486@gmail.com>
2023-12-29 18:05:44 +0300

    Certificate Crud

--- Business/Abstracts/ICertificateService.cs
+++ Business/Abstracts/ICertificateService.cs
@@ -1,8 +1,16 @@
-﻿namespace Business.Abstracts
+﻿using Business.Dtos.Certificate.Request;
+using Business.Dtos.Certificate.Response;
+using Core.DataAccess.Paging;
+
+namespace Business.Abstracts
 {
     public interface ICertificateService
     {
-        // İlgili metotlar
+        Task<CreatedCertificateResponse> AddAsync(CreateCertificateRequest createCertificateRequest);
+        Task<DeletedCertificateResponse> DeleteAsync(DeleteCertificateRequest deleteCertificateRequest);
+        Task<UpdatedCertificateResponse> UpdateAsync(UpdateCertificateRequest updateCertificateRequest);
+        Task<IPaginate<GetListCertificateResponse>> GetListAsync(PageRequest pageRequest);
+        Task<GetByIdCertificateResponse> GetByIdAsync(Guid id);
     }
 
 }
