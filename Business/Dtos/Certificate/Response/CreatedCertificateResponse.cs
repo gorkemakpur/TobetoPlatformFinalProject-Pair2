7d14418b07c55945e56628f4b1440f561da7177d
umudguny <ug152486@gmail.com>
2023-12-29 18:05:44 +0300

    Certificate Crud

---
+++ Business/Dtos/Certificate/Response/CreatedCertificateResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Dtos.Certificate.Response
+{
+    public class CreatedCertificateResponse
+    {
+        public Guid Id { get; set; }
+        public Guid StudentId { get; set; }
+        public string Url { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Business/Profiles/CertificateMappingProfile.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Business.Dtos.Certificate.Request;
+using Business.Dtos.Certificate.Response;
+using Core.DataAccess.Paging;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Profiles
+{
+    public class CertificateMappingProfile : Profile
+    {
+        public CertificateMappingProfile()
+        {
+
+            //create
+            CreateMap<Certificate, CreateCertificateRequest>().ReverseMap();
+            CreateMap<Certificate, CreatedCertificateResponse>().ReverseMap();
+            CreateMap<CreatedCertificateResponse, CreateCertificateRequest>().ReverseMap();
+            //list
+            CreateMap<Certificate, GetListCertificateResponse>().ReverseMap();
+            CreateMap<Certificate, GetByIdCertificateResponse>().ReverseMap();
+            CreateMap<IPaginate<Certificate>, Paginate<GetListCertificateResponse>>();
+            //delete
+            CreateMap<DeleteCertificateRequest, DeletedCertificateResponse>().ReverseMap();
+            //update
+            CreateMap<Certificate, UpdateCertificateRequest>().ReverseMap();
+            CreateMap<UpdateCertificateRequest, UpdatedCertificateResponse>().ReverseMap();
+        }
+    }
+}
