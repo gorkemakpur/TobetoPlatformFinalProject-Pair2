934f9a8ab00608aa7d7e1c1d65bc726de90ffb23
Ali Sefa AKBULUT <148756073+AkbulutAli@users.noreply.github.com>
2023-12-29 22:12:17 +0300

    Social media is cruds

---
+++ Business/Dtos/SocialMedia/Response/CreatedSocialMediaResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Dtos.SocialMedia.Response
+{
+    public class CreatedSocialMediaResponse
+    {
+        public Guid StudentId { get; set; }
+        public Guid SocialMediaNameId { get; set; }
+        public string Url { get; set; }
+    }
+
+}
---
+++ DataAccess/EntityConfiguration/SocialMediaConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.EntityConfiguration
+{
+    public class SocialMediaConfiguration : IEntityTypeConfiguration<SocialMedia>
+    {
+        public void Configure(EntityTypeBuilder<SocialMedia> builder)
+        {
+            //Tablo
+            builder.ToTable("SocialMedias").HasKey(i => i.Id);
+            //Sütunlar
+            builder.Property(i => i.Id).HasColumnName("Id").IsRequired();
+            builder.Property(i => i.SocialMediaNameId).HasColumnName("SocialMediaNameId").IsRequired();
+            builder.Property(i => i.StudentId).HasColumnName("StudentId").IsRequired();
+            builder.Property(i => i.Url).HasColumnName("Url").IsRequired();
+          
+
+            //İlişki
+            builder.HasOne(i => i.Students);
+            builder.HasOne(i => i.SocialMediaName);
+
+           
+
+            builder.HasQueryFilter(i => !i.DeletedDate.HasValue);
+        }
+    }
+
+}
