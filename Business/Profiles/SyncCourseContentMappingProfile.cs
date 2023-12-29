4968eef367780a092bef0c3357484c70c3c1d978
umudguny <ug152486@gmail.com>
2023-12-29 18:07:26 +0300

    SyncCourseContent Crud

---
+++ Business/Profiles/SyncCourseContentMappingProfile.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Core.DataAccess.Paging;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Dtos.SyncCourseContent.Response;
+using Business.Dtos.SyncCourseContent.Request;
+
+
+namespace Business.Profiles
+{
+    public class SyncCourseContentMappingProfile : Profile
+    {
+        public SyncCourseContentMappingProfile()
+        {
+
+            //create
+            CreateMap<SyncCourseContent, CreateSyncCourseContentRequest>().ReverseMap();
+            CreateMap<SyncCourseContent, CreatedSyncCourseContentResponse>().ReverseMap();
+            CreateMap<CreatedSyncCourseContentResponse, CreateSyncCourseContentRequest>().ReverseMap();
+            //list
+            CreateMap<SyncCourseContent, GetListSyncCourseContentResponse>().ReverseMap();
+            CreateMap<SyncCourseContent, GetByIdSyncCourseContentResponse>().ReverseMap();
+            CreateMap<IPaginate<SyncCourseContent>, Paginate<GetListSyncCourseContentResponse>>();
+            //delete
+            CreateMap<DeleteSyncCourseContentRequest, DeletedSyncCourseContentResponse>().ReverseMap();
+            //update
+            CreateMap<SyncCourseContent, UpdateSyncCourseContentRequest>().ReverseMap();
+            CreateMap<UpdateSyncCourseContentRequest, UpdatedSyncCourseContentResponse>().ReverseMap();
+        }
+    }
+}
