a6c5a082a5dfe8361f1b31de79678400e56af066
umudguny <ug152486@gmail.com>
2023-12-29 18:04:25 +0300

    AsyncCourseDepartment Crud

---
+++ Business/Profiles/AsynCourseDepartmentMappingProfile.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Business.Dtos.AsyncCourseDepartment.Request;
+using Business.Dtos.AsyncCourseDepartment.Response;
+using Core.DataAccess.Paging;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Profiles
+{
+    public class AsyncCourseDepartmentMappingProfile : Profile
+    {
+        public AsyncCourseDepartmentMappingProfile()
+        {
+
+            //create
+            CreateMap<AsyncCourseDepartment, CreateAsyncCourseDepartmentRequest>().ReverseMap();
+            CreateMap<AsyncCourseDepartment, CreatedAsyncCourseDepartmentResponse>().ReverseMap();
+            CreateMap<CreatedAsyncCourseDepartmentResponse, CreateAsyncCourseDepartmentRequest>().ReverseMap();
+            //list
+            CreateMap<AsyncCourseDepartment, GetListAsyncCourseDepartmentResponse>().ReverseMap();
+            CreateMap<AsyncCourseDepartment, GetByIdAsyncCourseDepartmentResponse>().ReverseMap();
+            CreateMap<IPaginate<AsyncCourseDepartment>, Paginate<GetListAsyncCourseDepartmentResponse>>();
+            //delete
+            CreateMap<DeleteAsyncCourseDepartmentRequest, DeletedAsyncCourseDepartmentResponse>().ReverseMap();
+            //update
+            CreateMap<AsyncCourseDepartment, UpdateAsyncCourseDepartmentRequest>().ReverseMap();
+            CreateMap<UpdateAsyncCourseDepartmentRequest, UpdatedAsyncCourseDepartmentResponse>().ReverseMap();
+        }
+    }
+}
---
+++ DataAccess/EntityConfiguration/AsyncCourseDepartmentConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace DataAccess.EntityConfiguration
+{
+    public class AsyncCourseDepartmentConfiguration : IEntityTypeConfiguration<AsyncCourseDepartment>
+    {
+        public void Configure(EntityTypeBuilder<AsyncCourseDepartment> builder)
+        {
+            // Tablomuz
+            builder.ToTable("AsyncCourseDepartments").HasKey(acd => acd.Id);
+
+            // Sütunlar
+            builder.Property(acd => acd.Id).HasColumnName("Id").IsRequired();
+            builder.Property(acd => acd.DepartmentId).HasColumnName("DepartmentId").IsRequired();
+            builder.Property(acd => acd.AsyncCourseId).HasColumnName("AsyncCourseId").IsRequired();
+            builder.Property(acd => acd.StartedDate).HasColumnName("StartedDate").IsRequired();
+            builder.Property(acd => acd.EndedDate).HasColumnName("EndedDate").IsRequired();
+
+            // Uniq Key(bakılacak)
+           // builder.HasIndex(indexExpression: acd => new { acd.DepartmentId, acd.AsyncCourseId }, name: "UK_AsyncCourseDepartments_DepartmentId_AsyncCourseId").IsUnique();
+
+            // İlişki
+            builder.HasOne(acd => acd.AsyncCourse)
+                .WithMany(ac => ac.AsyncCourseDepartments)
+                .HasForeignKey(acd => acd.AsyncCourseId);
+
+
+            builder.HasOne(acd => acd.Department)
+                .WithMany(d => d.AsyncCourseDepartments)
+                .HasForeignKey(acd => acd.DepartmentId);
+                
+
+            builder.HasQueryFilter(acd => !acd.DeletedDate.HasValue);
+        }
+    }
+}
