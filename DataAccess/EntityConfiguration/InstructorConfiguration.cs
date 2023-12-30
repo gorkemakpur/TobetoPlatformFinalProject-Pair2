b130022d7cbb1942f8bd7f9a777da9b30356ef7d
gorkemakpur <gorkemakpur@gmail.com>
2023-12-30 13:58:15 +0300

    InstructorConfiguration changed

--- DataAccess/EntityConfiguration/InstructorConfiguration.cs
+++ DataAccess/EntityConfiguration/InstructorConfiguration.cs
@@ -17,17 +17,11 @@ namespace DataAccess.EntityConfiguration
             builder.ToTable("Instructors").HasKey(i => i.Id);
             //Sütunlar
             builder.Property(i => i.Id).HasColumnName("Id").IsRequired();
-            builder.Property(i => i.FirstName).HasColumnName("FirstName").IsRequired();
-            builder.Property(i => i.LastName).HasColumnName("LastName").IsRequired();
-            builder.Property(i => i.Email).HasColumnName("Email").IsRequired();
-            builder.Property(i => i.Password).HasColumnName("Password").IsRequired();
-            builder.Property(i => i.ConfirmPassword).HasColumnName("ConfirmPassword").IsRequired();
-            builder.Property(i => i.PhoneNumber).HasColumnName("PhoneNumber").IsRequired();
-            builder.Property(i => i.About).HasColumnName("About").IsRequired();
-            //Uniq Key
-            builder.HasIndex(indexExpression: i => i.PhoneNumber, name: "UK_Instructors_PhoneNumber").IsUnique();
+            builder.Property(i => i.UserId).HasColumnName("UserId").IsRequired();
+
             //İlişki
             builder.HasMany(i => i.SyncCourseInstructors);
+            builder.HasOne(i => i.Users);
 
             builder.HasQueryFilter(i => !i.DeletedDate.HasValue); 
 
