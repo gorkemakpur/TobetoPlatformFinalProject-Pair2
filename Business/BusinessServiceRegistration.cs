12a7a193c4234a1b95d6cfb186f9d4a361f538af
gorkemakpur <gorkemakpur@gmail.com>
2024-01-17 19:13:48 +0300

    17.01.2024 19.13

--- Business/BusinessServiceRegistration.cs
+++ Business/BusinessServiceRegistration.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstracts;
 using Business.Concretes;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Business
 {
@@ -49,6 +50,21 @@ namespace Business
 
             //services.AddAutoMapper(Assembly.GetExecutingAssembly()); bunu burada yada program.cs içindeki haliyle yazabiliriz
 
+            return services;
+        }
+        public static IServiceCollection AddSubClassesOfType(
+    this IServiceCollection services,
+    Assembly assembly,
+    Type type,
+    Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null)
+        {
+            var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+            foreach (var item in types)
+                if (addWithLifeCycle == null)
+                    services.AddScoped(item);
+
+                else
+                    addWithLifeCycle(services, type);
             return services;
         }
     }
