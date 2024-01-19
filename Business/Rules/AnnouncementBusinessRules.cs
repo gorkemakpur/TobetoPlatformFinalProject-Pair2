db8580dddea72e128e34cdcdf28f7340577987d3
gorkemakpur <gorkemakpur@gmail.com>
2024-01-19 03:35:36 +0300

    Middleware optimization

--- Business/Rules/AnnouncementBusinessRules.cs
+++ Business/Rules/AnnouncementBusinessRules.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstracts;
 using Business.Dtos.Announcement.Response;
 using Core.Business.Rules;
+using Core.CrossCuttingConcerns.Exceptions.Types;
 using Core.DataAccess.Paging;
 using DataAccess.Abstracts;
 using Entities.Concretes;
@@ -25,15 +26,12 @@ namespace Business.Rules
             _announcementTypeService = announcementTypeService;
         }
         //rules örnek
-        public async Task CheckAnnouncementLimit(Guid id)
+        public async Task CheckAnnouncementLimit()
         {
-            var result = await _announcementDal.GetListAsync(
-                predicate: p=>p.AnnouncementTypeId==id,
-                size:10
-                );
-            if (result.Count >= 5)
+            var result = await _announcementDal.GetListAsync();
+            if (result.Count == 10)
             {
-                throw new Exception("Limit aşıldı");
+                throw new BusinessException("Limit aşıldı");
             }
         }
 
