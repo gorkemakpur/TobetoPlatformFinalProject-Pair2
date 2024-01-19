bc14b96a56c7cce157cf3f77fb210595ef3b0945
gorkemakpur <gorkemakpur@gmail.com>
2024-01-19 16:04:09 +0300

    jwt_V2

---
+++ Core/Utilities/Security/Encryption/SecurityKeyHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Encryption
+{
+    public class SecurityKeyHelper
+    {
+        //verdiğimiz security keyi byte array haline getiriyor appdevelopment
+        public static SecurityKey CreateSecurityKey(string security)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(security));
+        }
+    }
+}
