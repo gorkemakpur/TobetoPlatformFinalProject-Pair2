50142a5c7d7d379fc05368c87bf5e33a0c98bb2f
gorkemakpur <gorkemakpur@gmail.com>
2024-01-19 16:04:09 +0300

    jwt_V2

---
+++ Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Encryption
+{
+    public class SigningCredentialsHelper
+    {
+        //apinin gelen jwt yi doğrulaması gerektiği için bu kısımı kullanıyoruz
+        //sisteme girdiğin username ve parola bir credential örneği
+        public static SigningCredentials CreateSigningCredentials(SecurityKey security) 
+        {
+            return new SigningCredentials(security,SecurityAlgorithms.HmacSha512);
+        }
+    }
+}
---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Hashing
+{
+    public class HashingHelper
+    {
+        //ilk giriş parola oluştur
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+        //tekrar giriş parola kontrol
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != password[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
