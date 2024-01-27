b03c3085ec1e7c81ebc0720c1072857cb9d922f4
gorkemakpur <gorkemakpur@gmail.com>
2024-01-27 16:37:38 +0300

    StudentInformations dtos created

---
+++ Business/Dtos/StudentInformation/Response/CreatedStudentResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Business.Dtos.StudentInformation.Response;
+
+public class CreatedStudentInformationResponse
+{
+    public Guid Id { get; set; }
+    public Guid StudentId { get; set; }
+    public Guid DistrictId { get; set; }
+
+    public DateTime BirthDate { get; set; }
+    public string NationalityId { get; set; }
+    public int PhoneNumber { get; set; }
+    public string About { get; set; }
+    public string Country { get; set; }
+    public string AddressDescription { get; set; }
+}
