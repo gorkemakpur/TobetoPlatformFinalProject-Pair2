ea129490d391ecb07a69ddd2a45b4fd8b9a273e4
gorkemakpur <gorkemakpur@gmail.com>
2024-01-27 16:33:09 +0300

    StudentInformation table created. Student and information split 2 table

---
+++ DataAccess/Concretes/EntityFramework/EfStudentInformationDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess.Repositories;
+using DataAccess.Abstracts;
+using DataAccess.Context;
+using Entities.Concretes;
+
+namespace DataAccess.Concretes.EntityFramework
+{
+    public class EfStudentInformationDal : EfRepositoryBase<StudentInformation, Guid, TobetoPlatformFinalProjectContext>, IStudentInformationDal
+    {
+        public EfStudentInformationDal(TobetoPlatformFinalProjectContext context) : base(context)
+        {
+
+        }
+    }
+
+
+}
