2331f3a2b5c285372f5959bbbbcc20553a4e8a50
gorkemakpur <gorkemakpur@gmail.com>
2024-01-27 16:34:39 +0300

    api controller and operations added

--- WebApi/Controllers/AnnouncementUsersController.cs
+++ WebApi/Controllers/AnnouncementUsersController.cs
@@ -1,15 +1,50 @@
 using Business.Abstracts;
 using Business.Dtos.AnnouncementUser.Request;
+using Core.DataAccess.Paging;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers;
 
 public class AnnouncementUsersController : BaseController
 {
-    private readonly IAnnouncementUserService _announcementUser;
+    private readonly IAnnouncementUserService _AnnouncementUserService;
 
-    public AnnouncementUsersController(IAnnouncementUserService announcementUser)
+    public AnnouncementUsersController(IAnnouncementUserService AnnouncementUser)
     {
-        _announcementUser = announcementUser;
+        _AnnouncementUserService = AnnouncementUser;
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete([FromBody] DeleteAnnouncementUserRequest DeleteAnnouncementUserRequest)
+    {
+        var result = await _AnnouncementUserService.DeleteAsync(DeleteAnnouncementUserRequest);
+        return Ok(result);
+    }
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] CreateAnnouncementUserRequest createAnnouncementUserRequest)
+    {
+        var result = await _AnnouncementUserService.AddAsync(createAnnouncementUserRequest);
+        return Ok(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+    {
+        var result = await _AnnouncementUserService.GetListAsync(pageRequest);
+        return Ok(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetById([FromQuery] Guid id)
+    {
+        var result = await _AnnouncementUserService.GetByIdAsync(id);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update([FromQuery] UpdateAnnouncementUserRequest updateAnnouncementUserRequest)
+    {
+        var result = await _AnnouncementUserService.UpdateAsync(updateAnnouncementUserRequest);
+        return Ok(result);
     }
 }
