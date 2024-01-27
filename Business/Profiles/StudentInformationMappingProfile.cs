24371dd89cdf80a6df9db0c74a4a91f28fb945c5
gorkemakpur <gorkemakpur@gmail.com>
2024-01-27 16:39:47 +0300

    studentInformation profile added

---
+++ Business/Profiles/StudentInformationMappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Business.Dtos.StudentInformation.Request;
+using Business.Dtos.StudentInformation.Response;
+using Core.DataAccess.Paging;
+using Entities.Concretes;
+
+namespace Business.Profiles
+{
+    public class StudentInformationMappingProfile : Profile
+    {
+        public StudentInformationMappingProfile()
+        {
+
+            //create
+            CreateMap<StudentInformation, CreateStudentInformationRequest>().ReverseMap();
+            CreateMap<StudentInformation, CreatedStudentInformationResponse>().ReverseMap();
+            CreateMap<CreatedStudentInformationResponse, CreateStudentInformationRequest>().ReverseMap();
+            //list
+            CreateMap<StudentInformation, GetListStudentInformationResponse>().ReverseMap();
+            CreateMap<StudentInformation, GetByIdStudentInformationResponse>().ReverseMap();
+            CreateMap<IPaginate<StudentInformation>, Paginate<GetListStudentInformationResponse>>();
+            //delete
+            CreateMap<DeleteStudentInformationRequest, DeletedStudentInformationResponse>().ReverseMap();
+            //update
+            CreateMap<StudentInformation, UpdateStudentInformationRequest>().ReverseMap();
+            CreateMap<UpdateStudentInformationRequest, UpdatedStudentInformationResponse>().ReverseMap();
+        }
+    }
+
+}
