a7775d2678458714cdd833a047ae527122c8d6e0
gorkemakpur <gorkemakpur@gmail.com>
2024-01-30 14:54:30 +0300

     

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -71,14 +71,14 @@ namespace WebApi
 
             var app = builder.Build();
 
+            app.UseCors();
+
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
-            app.UseCors();  
 
             app.UseHttpsRedirection();
 
