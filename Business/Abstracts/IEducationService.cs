33bfb1b647d36c9d4afb6a0d8f1d4eea6144f73c
Demet Özmen <147596982+demetOzmen@users.noreply.github.com>
2024-02-02 08:42:57 +0300

     

--- Business/Abstracts/IEducationService.cs
+++ Business/Abstracts/IEducationService.cs
@@ -1,8 +1,16 @@
-﻿namespace Business.Abstracts
+﻿using Business.Dtos.Education.Request;
+using Business.Dtos.Education.Response;
+using Core.DataAccess.Paging;
+
+namespace Business.Abstracts
 {
     public interface IEducationService
     {
-        // İlgili metotlar
+        Task<CreatedEducationResponse> AddAsync(CreateEducationRequest createEducationRequest);
+        Task<DeletedEducationResponse> DeleteAsync(DeleteEducationRequest deleteEducationRequest);
+        Task<UpdatedEducationResponse> UpdateAsync(UpdateEducationRequest updateEducationRequest);
+        Task<IPaginate<GetListEducationResponse>> GetListAsync();
+        Task<GetByIdEducationResponse> GetByIdAsync(Guid id);
     }
 
 }
