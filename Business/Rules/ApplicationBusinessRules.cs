57c29f80d0b6890dd66c12b964050fb092c28d0b
Demet Özmen <147596982+demetOzmen@users.noreply.github.com>
2024-02-02 16:19:55 +0300

    Application businessRule Added

---
+++ Business/Rules/ApplicationBusinessRules.cs
@@ -0,0 +1,43 @@
+﻿using Business.Messagess;
+using Core.Business.Rules;
+using Core.CrossCuttingConcerns.Exceptions.Types;
+using DataAccess.Abstracts;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Rules
+{
+    public class ApplicationBusinessRules : BaseBusinessRules
+    {
+        private readonly IApplicationDal _applicationDal;
+
+        public ApplicationBusinessRules(IApplicationDal applicationDal)
+        {
+            _applicationDal = applicationDal;
+        }
+
+        public Task EnsureApplicationIsNotNull(Application? application)
+        {
+            if (application == null)
+            {
+                throw new BusinessException(Messages.ApplicationIsNull);
+            }
+            return Task.CompletedTask;
+        }
+
+        public async Task VerifyApplicationExistsById(Guid id, CancellationToken cancellationToken)
+        {
+            Application? application = await _applicationDal.GetAsync(
+                predicate: a => a.Id == id, enableTracking: false, cancellationToken: cancellationToken
+            );
+            await EnsureApplicationIsNotNull(application);
+        }
+
+
+    }
+
+}
